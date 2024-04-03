a39261c8a56fc59fffaf7636acf7f41af7c1cda1
SanahSidhu <sanaa.02.march@gmail.com>
2024-04-03 00:11:20 -0700

    minor changes and comments

--- Data Collection/Answering.py
+++ Data Collection/Answering.py
@@ -1,29 +1,25 @@
-from openai import OpenAI
-from dotenv import load_dotenv
-import os
 import pandas as pd
 from tqdm import tqdm
-import time
 from LLMConnection import getLLMResponse
-
-load_dotenv()
-
-client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
-
 
 def get_final_answers(context, subproblems, final_question):
     systemInstructions = "You are a helpful bot that can answer reasoning questions based off board game sitations"
     responses = []
-    prompt = f"Here is the context of the reasoning question: \n{context}"
-    # Send the context to ChatGPT and get an initial response
-    getLLMResponse(systemInstructions, prompt)
 
+    prompt = f"Here is the context of the reasoning question: \n{context}"
+    
+    # Send the context to ChatGPT and get an initial response
+    initial_response = getLLMResponse(systemInstructions, prompt)
+    responses.append(initial_response)
+
+    #iterate over subproblems
     for subproblem in subproblems:
         prompt = f"Answer the following subproblem based on the context provided to you earlier: \n{subproblem}"
         chat_response = getLLMResponse(systemInstructions, prompt)
         responses.append(chat_response)
 
     final_prompt = f"Answer the final question below based on what we have discussed till now. Finally, also state wheather it is proved , disproved or unkown. \n{final_question}"
+    
     # Finally, ask the main question and get the response
     final_answer = getLLMResponse(systemInstructions, final_prompt)
 
@@ -37,12 +33,15 @@ def main():
     FINAL_ANS_DIR = './final_answers_output.json'
     saveDf = None
     
+    #iterate the rows of the dataframe, for each row call the get_final_asnwers function
     for index, row in tqdm(df.iterrows(), total=df.shape[0]):
         context = row['context']
         subproblems = row['llama-subproblems'].split('||')
         final_question = row['question']
         
         subproblem_responses, final_answer = get_final_answers(context, subproblems, final_question)
+        
+    #whatever Dennis did lol
         
         if saveDf is None:
             saveDf = pd.DataFrame(columns=df.columns.tolist() + ['subproblem_responses', 'final_answer'])
@@ -62,10 +61,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    # Load data
-    # get initial response
-    # break down questions using reasoning (custom model, LLAMA)
-# Iteratively answer the subquestions based on the model, context, and subquestions
-# get a final answer
-# compare
